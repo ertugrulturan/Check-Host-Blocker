8caf0a15d2e391a1e0bad0600d6e39995586d2f6
Ertuğrul TURAN <60829297+obirninja@users.noreply.github.com>
2020-04-12 08:55:02 +0300

    Create block.py

---
+++ block.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# Coded by Ertugrul TURAN / T13R
+# NinjaNetwork - www.Obir.Ninja
+import os
+import time
+#colors ands CONFIGS
+timer = time.sleep
+#ban 2020 check-host.net ban
+os.system("iptables -A INPUT -s 95.216.93.10/24 -j DROP")
+os.system("iptables -A INPUT -s 212.129.21.10/24 -j DROP")
+os.system("iptables -A INPUT -s 46.4.143.10/24 -j DROP")
+os.system("iptables -A INPUT -s 51.89.1.10/24 -j DROP")
+os.system("iptables -A INPUT -s 185.105.238.10/24 -j DROP")
+os.system("iptables -A INPUT -s 193.187.152.10/24 -j DROP")
+os.system("iptables -A INPUT -s 185.25.204.10/24 -j DROP")
+os.system("iptables -A INPUT -s 185.120.77.10/24 -j DROP")
+os.system("iptables -A INPUT -s 195.123.211.10/24 -j DROP")
+os.system("iptables -A INPUT -s 88.119.179.10/24 -j DROP")
+os.system("iptables -A INPUT -s 178.17.171.10/24 -j DROP")
+os.system("iptables -A INPUT -s 185.209.161.10/24 -j DROP")
+os.system("iptables -A INPUT -s 185.159.82.10/24 -j DROP")
+os.system("iptables -A INPUT -s 93.170.128.10/24 -j DROP")
+os.system("iptables -A INPUT -s 45.80.70.10/24 -j DROP")
+os.system("iptables -A INPUT -s 139.99.75.10/24 -j DROP")
+os.system("iptables -A INPUT -s 185.29.8.10/24 -j DROP")
+os.system("iptables -A INPUT -s 179.43.148.10/24 -j DROP")
+os.system("iptables -A INPUT -s 185.86.77.10/24 -j DROP")
+os.system("iptables -A INPUT -s 176.111.58.10/24 -j DROP")
+os.system("iptables -A INPUT -s 198.56.183.10/24 -j DROP")
+os.system("iptables -A INPUT -s 172.246.126.10/24 -j DROP")
+os.system("iptables -A INPUT -s 146.196.65.10/24 -j DROP")
+#BANNER
+        print("\n         ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+        timer(0.25)
+        print("         ┃             NinjaNetwork Project               ┃")
+        timer(0.25)
+        print("         ┃                                             ┃")
+        timer(0.25)
+        print("         ┃                                             ┃")
+        timer(0.25)
+        print("         ┃      check-host auto prohibitive            ┃")
+        timer(0.25)
+        print("         ┃      GITHUB: @obirninja                     ┃")
+        timer(0.25)
+        print("         ┃      Web: https://obir.ninja                ┃")
+        timer(0.25)
+        print("         ┃                                             ┃")
+        timer(0.25)
+        print("         ┃                                             ┃")
+        timer(0.25)
+        print("         ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n")
+        timer(0.25)
